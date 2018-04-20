a3076b46425fc32789f0df7752a1abffb5b9bb59
JC <zhanmi5200@qq.com>
2018-04-20 09:24:57 +0800

    updata framework

---
+++ CommonCryptoframework/CommonCrypto.framework/Headers/CommonCrypto.h
@@ -0,0 +1,19 @@
+//
+//  CommonCrypto.h
+//  CommonCrypto
+//
+//  Created by Ice on 2018/4/18.
+//  Copyright © 2018年 Ice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CommonCrypto.
+FOUNDATION_EXPORT double CommonCryptoVersionNumber;
+
+//! Project version string for CommonCrypto.
+FOUNDATION_EXPORT const unsigned char CommonCryptoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonCrypto/PublicHeader.h>
+
+
